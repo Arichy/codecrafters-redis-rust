9e286bf48ccd623e5f8854d9df1921f3c804bf1d
Arichy <arichyx@qq.com>
2025-08-29 09:56:41 +0800

    fix: RPUSH/LPUSH now add values to list first, then notify BLPOP clients

--- src/commands/lpush.rs
+++ src/commands/lpush.rs
@@ -12,52 +12,53 @@ pub async fn handle(params: &[&str], ctx: &CommandContext) -> Result<Option<Mess
     }
     
     let key = params[0];
-    let mut values: Vec<String> = params[1..].iter().map(|s| s.to_string()).collect();
+    let values: Vec<String> = params[1..].iter().map(|s| s.to_string()).collect();
     let db_index = *ctx.selected_db_index.read().await;
     
-    // Check if there are clients waiting for this key
-    while !values.is_empty() && ctx.blocking_manager.has_waiting_clients(key).await {
-        if let Some(client) = ctx.blocking_manager.pop_waiting_client(key).await {
-            // Send the value directly to the waiting client
-            let value = values.pop().unwrap();
-            let response = Message::Array(Array {
-                items: vec![
-                    Message::BulkString(BulkString {
-                        length: key.len() as isize,
-                        string: key.to_string(),
-                    }),
-                    Message::BulkString(BulkString {
-                        length: value.len() as isize,
-                        string: value,
-                    }),
-                ],
-            });
-            
-            let mut writer = client.writer.lock().await;
-            let _ = writer.send(response).await;
-        }
-    }
-    
-    // Add any remaining values to the list
-    if !values.is_empty() {
-        match ctx.store.lpush(db_index, key, values).await {
-            Ok(len) => Ok(Some(Message::Integer(Integer { value: len as i64 }))),
-            Err(e) => {
-                if e.to_string().contains("WRONGTYPE") {
-                    Ok(Some(Message::SimpleError(SimpleError {
-                        string: e.to_string(),
-                    })))
-                } else {
-                    Err(e)
+    // First, add values to the list
+    match ctx.store.lpush(db_index, key, values).await {
+        Ok(len) => {
+            // Then check if there are clients waiting for this key
+            while ctx.blocking_manager.has_waiting_clients(key).await {
+                if let Some(client) = ctx.blocking_manager.pop_waiting_client(key).await {
+                    // Try to pop a value for the waiting client
+                    match ctx.store.lpop(db_index, key, 1).await {
+                        Ok(popped_values) if !popped_values.is_empty() => {
+                            let value = &popped_values[0];
+                            let response = Message::Array(Array {
+                                items: vec![
+                                    Message::BulkString(BulkString {
+                                        length: key.len() as isize,
+                                        string: key.to_string(),
+                                    }),
+                                    Message::BulkString(BulkString {
+                                        length: value.len() as isize,
+                                        string: value.clone(),
+                                    }),
+                                ],
+                            });
+                            
+                            let mut writer = client.writer.lock().await;
+                            let _ = writer.send(response).await;
+                        }
+                        _ => {
+                            // No more values in the list
+                            break;
+                        }
+                    }
                 }
             }
+            
+            Ok(Some(Message::Integer(Integer { value: len as i64 })))
         }
-    } else {
-        // All values were given to waiting clients
-        // Return the current list length
-        match ctx.store.llen(db_index, key).await {
-            Ok(len) => Ok(Some(Message::Integer(Integer { value: len as i64 }))),
-            Err(_) => Ok(Some(Message::Integer(Integer { value: 0 }))),
+        Err(e) => {
+            if e.to_string().contains("WRONGTYPE") {
+                Ok(Some(Message::SimpleError(SimpleError {
+                    string: e.to_string(),
+                })))
+            } else {
+                Err(e)
+            }
         }
     }
 }
