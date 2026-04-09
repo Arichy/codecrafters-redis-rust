d33a9ecf16dd40e2dd1710ce517c86d91e65408a
Arichy <arichyx@qq.com>
2026-04-09 21:05:00 +0800

    codecrafters submit [skip ci]

--- src/commands/geo/mod.rs
+++ src/commands/geo/mod.rs
@@ -1,9 +1,12 @@
 use crate::{
-    commands::{sorted_set::zadd, CommandContext},
+    commands::{geo::encode::encode, sorted_set::zadd, CommandContext},
     message::{Message, SimpleError},
     rdb::Value,
 };
 use anyhow::{Context as AnyhowContext, Result};
+
+mod decode;
+mod encode;
 
 pub async fn add(ctx: &CommandContext, args: &[String]) -> Result<Option<Message>> {
     let key = &args[0];
@@ -11,7 +14,7 @@ pub async fn add(ctx: &CommandContext, args: &[String]) -> Result<Option<Message
     let la = &args[2];
     let member = &args[3];
 
-    let lo = match lo.parse::<f32>() {
+    let lo = match lo.parse::<f64>() {
         Ok(lo) if lo >= -180.0 && lo <= 180.0 => lo,
         _ => {
             return Ok(Some(Message::SimpleError(SimpleError {
@@ -20,7 +23,7 @@ pub async fn add(ctx: &CommandContext, args: &[String]) -> Result<Option<Message
         }
     };
 
-    let la = match la.parse::<f32>() {
+    let la = match la.parse::<f64>() {
         Ok(la) if la >= -85.05112878 && la <= 85.05112878 => la,
         _ => {
             return Ok(Some(Message::SimpleError(SimpleError {
@@ -29,7 +32,9 @@ pub async fn add(ctx: &CommandContext, args: &[String]) -> Result<Option<Message
         }
     };
 
-    let zadd_args = &[key.to_string(), "0".to_string(), member.to_string()];
+    let score = encode(la, lo);
+
+    let zadd_args = &[key.to_string(), score.to_string(), member.to_string()];
 
     zadd(ctx, zadd_args).await
 }
